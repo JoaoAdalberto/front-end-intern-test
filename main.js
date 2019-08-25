793b8b4a024c284ae56c787f80a0710e18619c32
JoaoAdalberto <jadbnp18@gmail.com>
2019-08-25 16:49:57 -0300

    Código com a mágica aplicada
    Tela o mais parecido possível com a que estava como modelo.

--- main.js
+++ main.js
@@ -1,32 +1,45 @@
-// Adicionamos nosso código dentro de uma função anônima para evitar
-// variáveis globais.
+
 (function() {
-  // Elemento principal do formulário
   const contactForm = document.getElementById('contact');
 
-  // Campos do formulário
   const nameInput = document.getElementById('name');
   const emailInput = document.getElementById('email');
   const messageInput = document.getElementById('message');
 
-  // "addEventListener" instrui o navegador a executar uma função sempre
-  // que um evento específico acontece. Nesse caso, a função abaixo será
-  // executada toda vez que o botão de envio for pressionado.
+  
   contactForm.addEventListener('submit', function() {
-    // A propriedade "value" contém o valor atual de um input ou textarea
     const nameValue = nameInput.value;
+    const emailValue = emailInput.value;
+    const messageValue = messageInput.value;
+    
+    if(validateField(nameValue) && validateField(emailValue) && validateField (messageValue)){
+      window.alert(`Mensagem enviada com sucesso ${nameValue}`);
+    } else if (validateField(nameValue) == false && validateField(emailValue) == false && validateField(messageValue) == false) {
+      window.alert(`Preencha todos os campos para completar o envio da mensagem`)
+    } else if (validateField(nameValue) == true && validateField(emailValue) == false && validateField(messageValue) == false){
+      window.alert(`Preencha o campo do email e da mensagem para complentar o envio da mesma`)
+    } else if (validateField(nameValue) == false && validateField(emailValue) == true && validateField(messageValue) == false){
+      window.alert(`Preencha o campo do nome e da mensagem para complentar o envio da mesma`)
+    } else if (validateField(nameValue) == false && validateField(emailValue) == false && validateField(messageValue) == true){
+      window.alert(`Preencha o campo do nome e do email para complentar o envio da mensagem`)
+    } else if (validateField(nameValue) == true && validateField(emailValue) == true && validateField(messageValue) == false){
+      window.alert(`Preencha o campo da mensagem para complentar o envio da mesma`)
+    } else if (validateField(nameValue) == true && validateField(emailValue) == false && validateField(messageValue) == true){
+      window.alert(`Preencha o campo do email para complentar o envio da mensagem`)
+    } else if (validateField(nameValue) == false && validateField(emailValue) == true && validateField(messageValue) == true){
+      window.alert(`Preencha o campo do nome para complentar o envio da mensagem`)
+    }
 
-    // A propriedade "length" contém o tamanho (em caracteres) de uma
-    // uma string. Toda string contém essa propriedade.
-    const nameLength = nameValue.length;
 
-    // "window" é um objeto global que representa a janela (ou aba) do
-    // navegador que está executando o código do seu site. O método
-    // "alert" simplesmente mostra um aviso para o usuário contendo a
-    // mensagem provida.
-    window.alert(`Botão enviar clicado! Conteúdo do campo nome: ${nameValue} (${nameLength} caracteres)`);
-
-    // Altere e complete essa função para validar os campos do formulário
-    // de acordo com as especificações do teste. Boa sorte!
   });
+
+  function validateField(field){
+    isValid = false;
+
+    if(field != null && field != ''){
+      isValid = true;
+    }
+
+    return isValid;
+  }
 })();
